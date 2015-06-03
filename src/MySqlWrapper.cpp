a87424de1a7da32689367f48b65059cf557759ce
yangboz <youngwelle@gmail.com>
2015-06-03 12:41:10 +0800

    ADDED:first share of source code;

---
+++ src/MySqlWrapper.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : MySqlWrapper.cpp
+// Author      : youngwelle@gmail.com
+// Version     :
+// Copyright   : Copyright GODPAPER
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello,MySqlWrapper!!!" << endl; // prints !!!Hello,MySqlWrapper!!!
+	return 0;
+}
